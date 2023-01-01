69aebcabeee35f4a1b24b1cb7e5e37927dcfe453
Francis James Tolentino <francistolentino1107@gmail.com>
2023-01-02 00:25:50 +0800

    App bar

--- lib/screens/createTaskScreen.dart
+++ lib/screens/createTaskScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:taskeu/widgets/extended/exText.dart';
 
 class TaskCreationScreen extends StatefulWidget {
   const TaskCreationScreen({super.key});
@@ -13,9 +14,42 @@ class _TaskCreationScreenState extends State<TaskCreationScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: Header(),
       body: Center(
         child: Text('Create Task'),
       ),
     );
   }
+}
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                size: 18,
+              ),
+            ),
+            const ExText(
+              text: 'Create Schedule',
+              weight: FontWeight.w600,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(100);
 }
