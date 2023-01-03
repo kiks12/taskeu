f7aa716a081baae8215f4fcbbebfe15f845f89b7
Francis James Tolentino <francistolentino1107@gmail.com>
2023-01-03 22:05:52 +0800

    Create Schedule Working

--- lib/screens/createTaskScreen.dart
+++ lib/screens/createTaskScreen.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:intl/intl.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:taskeu/models/todo.dart';
 import 'package:taskeu/utils/date.dart';
 import 'package:taskeu/utils/taskUtils.dart';
 import 'package:taskeu/widgets/extended/exText.dart';
 import 'package:taskeu/widgets/task.dart';
 import 'package:taskeu/widgets/tasks.dart';
+
+import 'package:path/path.dart' as path;
+
+const String START_TIME = 'START_TIME';
+const String END_TIME = 'END_TIME';
 
 class TaskCreationScreen extends StatefulWidget {
   const TaskCreationScreen({
@@ -19,41 +25,234 @@ class TaskCreationScreen extends StatefulWidget {
 }
 
 class _TaskCreationScreenState extends State<TaskCreationScreen> {
-  List<Todo> todos = [
-    const Todo(
-      id: 0,
-      title: 'Title 1',
-      date: 1672675200000,
-      start: '7:00',
-      end: '8:00',
-      task: 'Create Blah',
-      status: 'Scheduled',
-    ),
-    const Todo(
-      id: 1,
-      title: 'Title 2',
-      date: 1672675200000,
-      start: '10:00',
-      end: '12:00',
-      task: 'Create Blah',
-      status: 'Scheduled',
-    ),
-    const Todo(
-      id: 1,
-      title: 'Lunch',
-      date: 1672675200000,
-      start: '12:00',
-      end: '13:00',
-      task: 'Eat Lunch',
-      status: 'Scheduled',
-    ),
-  ];
+  DateTime initialDate =
+      Date(date: DateTime.now()).date.add(const Duration(days: 1));
+  List<Todo> todos = [];
+  TimeOfDay? selectedStartTime = const TimeOfDay(hour: 7, minute: 0);
+  TimeOfDay? selectedEndTime = const TimeOfDay(hour: 8, minute: 0);
+  TimeOfDay? selectedTime = const TimeOfDay(hour: 8, minute: 0);
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  int currentID = 0;
 
   @override
   void initState() {
     super.initState();
-    todos = TaskUtils(todos: todos).todos;
+    if (todos.isNotEmpty) normalizeTodo();
+    getID();
+  }
+
+  void normalizeTodo() {
+    todos = TaskUtils(todos: todos, isCreating: true).todos;
     setState(() {});
+  }
+
+  Future<void> getID() async {
+    final Database db = await openDatabase(
+      path.join(await getDatabasesPath(), 'official_taskeu_2.db'),
+    );
+
+    currentID =
+        Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM todo'))
+            as int;
+    setState(() {});
+  }
+
+  // void normalizeTime(TimeOfDay time) {
+  // print(TimeOfDay(hour: 13, minute: 0));
+  // }
+
+  void addTask() {
+    if (formKey.currentState!.validate()) {
+      todos.add(
+        Todo(
+          id: currentID,
+          title: titleController.text,
+          date: initialDate.millisecondsSinceEpoch,
+          start: selectedStartTime!.format(context),
+          end: selectedEndTime!.format(context),
+          task: descriptionController.text,
+          status: 'Scheduled',
+        ),
+      );
+      currentID++;
+      normalizeTodo();
+      Navigator.pop(context);
+    }
+  }
+
+  Future<void> saveSchedule() async {
+    for (var todo in todos) {
+      if (todo.status == 'Free') continue;
+      todo.insertUserToDB();
+    }
+    Navigator.pop(context);
+  }
+
+  Future<void> selectStartTime() async {
+    selectedTime = await showTimePicker(
+      context: context,
+      initialTime: const TimeOfDay(hour: 0, minute: 0),
+    );
+  }
+
+  Future<void> handleStartTimeButtonClick(String type) async {
+    await selectStartTime();
+    if (selectedTime != null) {
+      if (type == START_TIME) selectedStartTime = selectedTime;
+      if (type == END_TIME) selectedEndTime = selectedTime;
+    }
+  }
+
+  void showAddTaskModal() async {
+    return await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(25.0))),
+      builder: ((context) {
+        return StatefulBuilder(
+          builder:
+              (BuildContext context, void Function(void Function()) setState) {
+            return Padding(
+              padding: MediaQuery.of(context).viewInsets,
+              child: Wrap(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ExText(
+                                text: (selectedStartTime != null)
+                                    ? 'Start Time: ${selectedStartTime!.format(context)}'
+                                    : 'Start Time:'),
+                            TextButton(
+                              onPressed: () async {
+                                await handleStartTimeButtonClick(START_TIME);
+                                setState(() {});
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Text('Select Time'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ExText(
+                                text: (selectedEndTime != null)
+                                    ? 'End Time: ${selectedEndTime!.format(context)}'
+                                    : 'End Time:'),
+                            TextButton(
+                              onPressed: () async {
+                                await handleStartTimeButtonClick(END_TIME);
+                                setState(() {});
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Text('Select Time'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Form(
+                            key: formKey,
+                            child: Column(
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const ExText(
+                                      text: 'Title',
+                                      size: 12,
+                                    ),
+                                    TextFormField(
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please fill in this field.';
+                                        }
+                                        return null;
+                                      },
+                                      controller: titleController,
+                                      decoration: const InputDecoration(
+                                          hintText: 'Title'),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 15),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const ExText(
+                                        text: 'Decription',
+                                        size: 12,
+                                      ),
+                                      TextFormField(
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please fill in this field.';
+                                          }
+                                          return null;
+                                        },
+                                        maxLines: 10,
+                                        controller: descriptionController,
+                                        decoration: const InputDecoration(
+                                            hintText: 'Description'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.black87),
+                              foregroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.white),
+                            ),
+                            onPressed: addTask,
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 50,
+                                vertical: 10,
+                              ),
+                              child: Text('Add Task'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      }),
+    );
   }
 
   @override
@@ -68,7 +267,15 @@ class _TaskCreationScreenState extends State<TaskCreationScreen> {
           ],
         ),
       ),
-      bottomNavigationBar: const SaveScheduleButton(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showAddTaskModal,
+        backgroundColor: Colors.black87,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: SaveScheduleButton(
+        saveSchedule: saveSchedule,
+      ),
     );
   }
 }
@@ -167,7 +374,12 @@ class _DateSelectorState extends State<DateSelector> {
 }
 
 class SaveScheduleButton extends StatefulWidget implements PreferredSizeWidget {
-  const SaveScheduleButton({super.key});
+  const SaveScheduleButton({
+    super.key,
+    required this.saveSchedule,
+  });
+
+  final Future<void> Function() saveSchedule;
 
   @override
   State<SaveScheduleButton> createState() => _SaveScheduleButtonState();
@@ -203,7 +415,7 @@ class _SaveScheduleButtonState extends State<SaveScheduleButton> {
               backgroundColor: MaterialStateProperty.all<Color>(Colors.black87),
               foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
             ),
-            onPressed: () {},
+            onPressed: widget.saveSchedule,
             child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
               child: ExText(
